fd603db5a402ba2abdf6b2d0fa9c83e74336f9b9
LilyLinh <linhhacaothuy@gmail.com>
2024-04-17 23:50:53 +0100

    Create FoodStorageServer and FoodStorageServiceImpl

---
+++ IdeaProjects/FoodContainer/src/main/java/org/example/FoodStorageServiceImpl.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import io.grpc.stub.StreamObserver;
+import org.example.foodcontainer.foodstorageservice.FoodStorageServiceGrpc;
+import org.example.foodcontainer.foodstorageservice.FoodStorageServiceRequest;
+import org.example.foodcontainer.foodstorageservice.FoodStorageServiceResponse;
+import org.example.foodcontainer.foodstorageservice.FoodStorageServiceResponseManyTimes;
+
+public class FoodStorageServiceImpl extends FoodStorageServiceGrpc.FoodStorageServiceImplBase {
+}
